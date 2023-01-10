aff81928a6c69c7b88f3ec706becb790562ad9ea
Stackbit Code Editor <projects@stackbit.com>
2023-01-10 17:14:39 +0200

    stackbit.config.js: changed

--- stackbit.config.js
+++ stackbit.config.js
@@ -96,6 +96,7 @@ export default {
         CtaSection: {
             name: 'CtaSection',
             localized: true,
+            type: 'data',
             fields: [
                 {
                     "type": "enum",
