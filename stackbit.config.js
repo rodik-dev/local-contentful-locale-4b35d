a98422c9dda8a35c2bd9a37f87c85b2214ae4669
Stackbit Code Editor <projects@stackbit.com>
2023-01-10 16:50:06 +0200

    stackbit.config.js: changed

--- stackbit.config.js
+++ stackbit.config.js
@@ -25,7 +25,7 @@ class MyContentSource extends ContentfulContentSource {
             options.locale = locale;
             const localeField = options.model.fields.find(field => field.name === 'locale');
             if (localeField) {
-                options.updateOperationFields.locale = {value: locale}
+                options.updateOperationFields.locale = {value: locale};
             }
         }
         return super.createDocument(options);
