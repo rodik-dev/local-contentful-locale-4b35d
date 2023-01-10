ca99b1ffabb76f23f9b56d610d6333c440d59846
Stackbit Code Editor <projects@stackbit.com>
2023-01-10 16:50:44 +0200

    stackbit.config.js: changed

--- stackbit.config.js
+++ stackbit.config.js
@@ -21,6 +21,7 @@ console.log("DEBUG:: stackbit.config.js init");
 class MyContentSource extends ContentfulContentSource {
 
     async createDocument(options, locale = 'en-US') {
+        console.log("RUNNING USER CODE CREATE DOC", {options, locale})
         if (options.model.localized) {
             options.locale = locale;
             const localeField = options.model.fields.find(field => field.name === 'locale');
