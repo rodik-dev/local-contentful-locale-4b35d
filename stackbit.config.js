70b430a3df4ac006760c81cba86da5a10b5ff77c
Stackbit Code Editor <projects@stackbit.com>
2023-01-10 18:33:44 +0200

    stackbit.config.js: changed

--- stackbit.config.js
+++ stackbit.config.js
@@ -101,7 +101,7 @@ export default {
     // appropriate page fields for editing as you navigate between fields.
     models: {
         page: { type: 'page', urlPath: '/{slug}' },
-        localizedPage: { type: 'page', urlPath: '/{slug}', locaized:l true },
+        localizedPage: { type: 'page', urlPath: '/{slug}', locaized: true },
         CtaSection: {
             fields: [
                 {
