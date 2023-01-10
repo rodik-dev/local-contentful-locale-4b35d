4dd8ccf53fca2721bb81745a3535116526f3c54f
Stackbit Code Editor <projects@stackbit.com>
2023-01-10 17:58:14 +0200

    stackbit.config.js: changed

--- stackbit.config.js
+++ stackbit.config.js
@@ -29,7 +29,7 @@ class MyContentSource extends ContentfulContentSource {
                 options.updateOperationFields.locale = {  type: 'enum', value: options.locale};
             }
 
-            const slugField = options.model.fields.find(field => field.name === 'locale');
+            const slugField = options.model.fields.find(field => field.name === 'slug');
         }
         return super.createDocument(options);
     }
