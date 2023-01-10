516fd3722b9acabe7b399f07bf41d093b89cdabb
Stackbit Code Editor <projects@stackbit.com>
2023-01-10 18:00:49 +0200

    stackbit.config.js: changed

--- stackbit.config.js
+++ stackbit.config.js
@@ -31,7 +31,7 @@ class MyContentSource extends ContentfulContentSource {
 
             const slugField = options.model.fields.find(field => field.name === 'slug');
             if (slugField) {
-                if (!options.updateOperationFields?.slug?.value.startsWith(locale)) {
+                if (options.updateOperationFields?.slug? && !options.updateOperationFields?.slug?.value.startsWith(locale)) {
                     throw new Error(`Slug '${options.updateOperationFields?.slug?.value}' must start with locale '${locale}'`)
                 }
             }
