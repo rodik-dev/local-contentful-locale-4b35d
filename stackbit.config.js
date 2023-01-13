ae9b8eb0e36c918dcb2daa8ff475b6d0be97d1d1
Stackbit Code Editor <projects@stackbit.com>
2023-01-13 11:15:32 +0200

    stackbit.config.js: changed

--- stackbit.config.js
+++ stackbit.config.js
@@ -32,7 +32,7 @@ class MyContentSource extends ContentfulContentSource {
             )
             if (slugField) {
                 if (options.updateOperationFields?.slug === "oops") {
-                    throw 'you cannot create oops';
+                    throw 'you cannot create oops'
                 }
                 if (options.updateOperationFields?.slug && !options.updateOperationFields?.slug?.value.startsWith(options.locale)) {
                     throw new Error(`slug '${options.updateOperationFields?.slug?.value}' must start with locale '${options.locale}'`);
