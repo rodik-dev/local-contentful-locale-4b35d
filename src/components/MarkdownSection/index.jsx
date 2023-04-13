afa864222d0513559565846fa87982562a390064
Stackbit Code Editor <projects@stackbit.com>
2023-04-13 18:44:18 +0000

    index.jsx: changed

--- src/components/MarkdownSection/index.jsx
+++ src/components/MarkdownSection/index.jsx
@@ -4,14 +4,12 @@ import Container from '../Container';
 const MarkdownSection = (props) => {
     return (
         <Container data-sb-field-path={props.path}>
-            <div data-sb-field-path=".markdown">
-                {props.fields.markdown && (
+            {props.fields.markdown && (<div data-sb-field-path=".markdown">
                 <ReactMarkdown>
                     {props.fields.markdown}
-                </ReactMarkdown>
-                )}
-                
+                </ReactMarkdown>  
             </div>
+            )}
             <div data-sb-field-path=".rich">
                 {props.fields.rich && documentToReactComponents(fields.test, options)}
             </div>
