b817a2654dd544cf527eecc910ab2422c440a135
Stackbit Code Editor <projects@stackbit.com>
2023-04-13 09:50:48 +0000

    index.jsx: changed

--- src/components/HeroBanner/index.jsx
+++ src/components/HeroBanner/index.jsx
@@ -19,7 +19,7 @@ const HeroBanner = (props) => {
             <Container isSection={false} classes={styles[fields.textColor]}>
                 {fields.title && <h1 data-sb-field-path=".title" className={styles.hero__title}>{fields.title}</h1>}
                 {fields.subtitle && <p data-sb-field-path=".subtitle" className={styles.hero__subtitle}>{fields.subtitle}</p>}
-                {fields.test && <p data-sb-field-path=".subtitle" className={styles.hero__subtitle}>{fields.subtitle}</p>}
+                {fields.test && <div data-sb-field-path=".subtitle" className={styles.hero__subtitle}>{fields.subtitle}</p>}
             </Container>
         </section>
     );
