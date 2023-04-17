5fb084a6371db5bf7f4d788e9f6ddeefd795d5e2
Stackbit Code Editor <projects@stackbit.com>
2023-04-17 17:31:53 +0000

    stackbit.config.js: changed

--- stackbit.config.js
+++ stackbit.config.js
@@ -7,8 +7,8 @@ import { defineStackbitConfig } from '@stackbit/types';
 // For local development, share the .env file that the Next.js server reads
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-// const LOCALIZED_MODELS = ['localizedPage', 'CtaSection', 'MarkdownSection'];
-const LOCALIZED_MODELS = ['CtaSection', 'MarkdownSection'];
+const LOCALIZED_MODELS = ['localizedPage', 'CtaSection', 'MarkdownSection'];
+// const LOCALIZED_MODELS = ['CtaSection', 'MarkdownSection'];
 const LOCALES = ['en-US', 'he']
 const getDocumentLocale = (document) => {
     if (document.fields.slug) {
